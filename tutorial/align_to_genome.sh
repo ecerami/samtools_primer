e3ebd757660f57c06a581356f367f6b3e8c64238
Ethan Cerami <cerami@gmail.com>
2013-05-04 20:48:05 -0400

    Updated to use bowtie2

--- tutorial/align_to_genome.sh
+++ tutorial/align_to_genome.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # First, make sure bowtie is installed
-if ! type bowtie > /dev/null 2>&1; then
-    echo "Could not find bowtie.  Please adjust your path or download from:  http://bowtie-bio.sourceforge.net/index.shtml"
+if ! type bowtie2 > /dev/null 2>&1; then
+    echo "Could not find bowtie2.  Please adjust your path or download from:  http://bowtie-bio.sourceforge.net/bowtie2/index.shtml"
 fi
 
 # Align the simulated reads against the reference genome
-bowtie -S e_coli simulated_reads/sim_reads.fq alignments/sim_reads_aligned.sam
+bowtie2 -x indexes/e_coli -U simulated_reads/sim_reads.fq -S alignments/sim_reads_aligned.sam
